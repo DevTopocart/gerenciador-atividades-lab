fa3f14f44b41249a7122f029a3a36ab86426d983
Victor Marotta <victordossantosmarotta@gmail.com>
2023-04-24 08:54:12 -0300

    chore: alterado url e chave da api do redmine

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,40 +1,15 @@
 import axios from "axios";
-import { invoke } from "@tauri-apps/api";
 import axiosTauriApiAdapter from "axios-tauri-api-adapter";
 
 const api = axios.create({
   adapter: axiosTauriApiAdapter,
-  baseURL: "https://39c4756f97.bigus-e1.easyproject.com",
+  baseURL: "https://topocart.easyredmine.com",
   headers: {
     "Content-Type": "application/json",
   },
   params: {
-    key: "d88f0ec1f38eabd194ce3058a970c53a98b539d5",
+    key: "a9260e03ecd57a3cec7b7cffa3dce43561febc16",
   },
 });
-
-// api.interceptors.request.use(async (config: any) => {
-//   try {
-//     const response = await invoke("https", {
-//       method: config.method,
-//       url: config.url,
-//       options: {
-//         headers: config.headers,
-//         data: config.data,
-//         params: config.params,
-//       },
-//     });
-
-//     return {
-//       ...config,
-//       data: response,
-//       headers: {},
-//       status: 200,
-//       statusText: "OK",
-//     };
-//   } catch (error) {
-//     return Promise.reject(error);
-//   }
-// });
 
 export default api;
