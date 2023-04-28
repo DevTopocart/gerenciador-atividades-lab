54cde630b89203be1ab952a2cafc0c5b2c713dbb
Victor Marotta <victordossantosmarotta@gmail.com>
2023-04-28 11:51:33 -0300

    fix: corrigido versionamento da aplicacao

--- release.config.js
+++ release.config.js
@@ -1,20 +1,35 @@
+const { execSync } = require('child_process');
+
 module.exports = {
-    branches: ['develop'],
-    repositoryUrl: 'https://github.com/DevTopocart/apontador-horas',
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/github',
-      [
-        '@semantic-release/npm',
-        {
-          npmPublish: false
-        }
-      ],
-      [
-        '@semantic-release/git', {
-        'assets': ['package.json'],
+  branches: ['develop'],
+  repositoryUrl: 'https://github.com/paschendale/webgis-itabirito',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false
+      }
+    ],
+    [
+      '@semantic-release/git', {
+        'assets': ['app/package.json', 'src-tauri/tauri.conf.json'],
         'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-      }],
+      }
+    ],
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: () => {
+          const version = require('./src-tauri/tauri.conf.json').package.version;
+          const newVersion = semanticRelease.getNextVersion(version, 'minor');
+          const command = `npx json -I -f src-tauri/tauri.conf.json -e "this.package.version='${newVersion}'"`;
+          execSync(command);
+        }
+      }
     ]
-  }
+  ],
+  pkgRoot: 'app'
+};
