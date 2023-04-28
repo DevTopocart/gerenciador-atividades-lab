406a0d86886d44629111ff7604cc17aefbb3276e
Victor Marotta <victordossantosmarotta@gmail.com>
2023-04-28 11:56:50 -0300

    chore: adicionado branch no release.config

--- release.config.js
+++ release.config.js
@@ -1,7 +1,7 @@
 const { execSync } = require('child_process');
 
 module.exports = {
-  branches: ['develop'],
+  branches: ['develop','technical/ci-enhancements'],
   repositoryUrl: 'https://github.com/paschendale/webgis-itabirito',
   plugins: [
     '@semantic-release/commit-analyzer',
