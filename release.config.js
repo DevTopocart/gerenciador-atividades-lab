bbe06af2f2c254ba1c3db70eaa1e0e918525b597
moacirmsj <moacirmascarenha86@gmail.com>
2023-10-18 14:52:37 -0300

    chore: ajusta versao release

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ["develop"],
+  branches: ["main"],
   repositoryUrl: "https://github.com/DevTopocart/apontador-horas",
   plugins: [
     "@semantic-release/commit-analyzer",
